7d19a720d38abcad116cef0b8a8d4d77320d52ef
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-01-31 20:22:49 +0100

    new2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = "<h1>Hod kostkou</h1>"
+document.body.innerHTML += "<h1>Hod kostkou</h1>"
 
 let cislo = Math.floor(Math.random()*6)+1
-document.body.innerHTML = cislo
+document.body.innerHTML += cislo
