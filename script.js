6ac41c6a42b194627d74396b95481a54da236109
Gabriela Dyworova <gabriela.dyworova@gmail.com>
2024-02-09 16:52:56 +0100

    uprava

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.innerHTML = "<h1>Hod kostkou</h1>"
+document.body.innerHTML = "<h1>Hod kostkou</h1>"
 
 let cislo = Math.floor(Math.random()*6)+1
-document.innerHTML = cislo
+document.body.innerHTML = cislo
